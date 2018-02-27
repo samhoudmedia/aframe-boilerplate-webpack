4cb7df554e6fd49bf93cbef0e12240b41044fe57
dandevri <hi@dandevri.es>
2018-02-27 15:30:18 +0100

    Fix webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
 };
 
 module.exports = {
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
   },
   output: {
     path: PATHS.build,
-    filename: '[name].js'
+    filename: '[name].js',
   },
   plugins: [
-    new HtmlWebpackPlugin({})
+    new HtmlWebpackPlugin({
+      title: 'A-Frame Boilerplate',
+    }),
   ],
-}
+};
